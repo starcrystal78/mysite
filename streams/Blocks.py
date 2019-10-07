72283a68da7b1ed91682167852ee6d3eb772a902
Dipendra p. Pandey <dppandey@stral.local>
2019-10-07 13:38:05 +0530

    this is the commot of day 7/10

--- streams/Blocks.py
+++ streams/Blocks.py
@@ -1,8 +1,9 @@
 """stream fields live here"""
 from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
 
 
-class TitleAndTextBlock(blocks.StaticBlock):
+class TitleAndTextBlock(blocks.StructBlock):
     """Title and header only"""
     title = blocks.CharBlock(required=True, help_text="Add your Title")
     text = blocks.TextBlock(required=True, help_text="Add additional Text")
@@ -11,3 +12,44 @@ class TitleAndTextBlock(blocks.StaticBlock):
         template = "streams/title_and_text_block.html"
         icon = "edit"
         label = "Title & Text "
+
+class CardBlock(blocks.StructBlock):
+    """cads with image and text and buttons"""
+    title = blocks.CharBlock(required=True, help_text="Add uour title")
+    cards - blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("image", ImageChooserBlock(required=True)),
+                ("Titel", blocks.CharBlock(required=True,  max_length=40)),
+                ("Text", blocks.TextBlock(required=True, max_length=200))
+            ]
+        )
+    )
+
+
+
+class RichtextBLock (blocks.RichTextBlock):
+
+    class Meta:
+        template = "streams/richtext_block.html"
+        icon = "doc-full"
+        label = 'Full RichText'
+
+
+class SimpleRichtextBLock (blocks.RichTextBlock):
+
+    def __init__(self, required=True, help_text=None, editor='default', features=None, validators=(), **kwargs):
+        super().__init__(**kwargs)
+        self.features = [
+            "bold",
+            "italic",
+            'link',
+        ]
+
+
+
+
+    class Meta:
+        template = "streams/simplerichtext_block.html"
+        icon  = "edit"
+        label = 'SimpleRichText'
--- streams/models.py
+++ streams/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from modelcluster.fields import ParentalKey
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField, StreamField
+from wagtail.admin.edit_handlers import StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
 # New imports added for ClusterTaggableManager, TaggedItemBase, MultiFieldPanel
@@ -11,17 +12,22 @@ from taggit.models import TaggedItemBase
 from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
 from . import Blocks
 
-class BlogTagIndexPage(Page):
-    template = ''
-    subtitle = models.CharField(max_length=120, blank=False, null=True)
+
+class FlexPage(Page):
+    template = 'streams\Flex_page.html'
     content = StreamField(
         [
-            ("title_and_text", Blocks.TitleAndTextBlock())
-        ]
+            ("title_and_text", Blocks.TitleAndTextBlock()),
+            ("Full_richtext", Blocks.RichtextBLock()),
+            ("simple_richtext", Blocks.SimpleRichtextBLock()),
+        ],
+        null=True,
+        blank=True,
     )
+    subtitle = models.CharField(max_length=120, blank=False, null=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
-        StreamField('content'),
+        StreamFieldPanel('content'),
         ]
 
